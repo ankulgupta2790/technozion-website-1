56aecff13ca6b68b57de4c8f5b75ed0efe6d452a
Ankul Gupta <128960413+ankulgupta2790@users.noreply.github.com>
2024-10-03 05:56:34 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./Hero";
+import { About } from "./About";
+
+const Home = (props) => {
+    return (
+        <>
+            <Hero />
+        </>
+    )
+}
+
+export default Home;
