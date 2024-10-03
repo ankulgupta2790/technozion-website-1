0bf645748a983a01b52cf533de3be0b0ad325aa5
Ankul Gupta <128960413+ankulgupta2790@users.noreply.github.com>
2024-10-03 05:58:38 +0530

    Add files via upload

---
+++ src/Context/RoutesManager.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { About } from '../components/About';
+import AuthPage from '../components/Authpage/AuthPage';
+import Home from '../components/Home';
+import Sponsors from '../components/Sponsors/Sponsors';
+import { useAuth } from './AuthManager';
+import { Register } from '../components/Register';
+import { useLocation } from 'react-router-dom';
+
+const RoutesManager = () => {
+	const { user } = useAuth();
+	const location = useLocation();
+	window.scroll(0,0);
+	return (
+		<Routes>
+			{!user ? (
+				<>
+					<Route path="/auth" element={<AuthPage />} />
+					<Route path="/register" element={<AuthPage />} />
+				</>
+			) : (
+				<>
+					<Route path="/auth" element={<Register />} />
+					<Route path="/register" element={<Register />} />
+				</>
+			)}
+
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/sponsors" element={<Sponsors />} />
+		</Routes>
+	);
+};
+export default RoutesManager;
