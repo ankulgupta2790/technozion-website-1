ee97d6fdc61733c0fc7390cfd5c35f10873bdf56
Ankul Gupta <128960413+ankulgupta2790@users.noreply.github.com>
2024-10-03 05:54:55 +0530

    Add files via upload

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { ImCross } from "react-icons/im";
+import chota_logo from "./chota_logo.png";
+import './index.css';
+
+const oldNavigation = [
+	{ name: "HOME", link: "/" },
+	{ name: "ABOUT", link: "/about" },
+];
+
+export default function Navbar() {
+	const [navigation, setNavigation] = useState(oldNavigation);
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 725);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobileView(window.innerWidth <= 725);
+		};
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	useEffect(() => {
+		if (isMobileView) {
+			setNavigation([...oldNavigation, { name: "REGISTER", link: "/auth" }]);
+		} else {
+			setNavigation(oldNavigation);
+		}
+	}, [isMobileView]);
+
+	const closeMenu = () => {
+		setMenuOpen(false);
+	};
+
+	const listItems = navigation.map((menuItem, index) => (
+		<li key={index}>
+			<NavLink to={menuItem.link} onClick={() => closeMenu()}>{menuItem.name}</NavLink>
+		</li>
+	));
+
+	return (
+		<>
+			{!menuOpen ? (
+				<div className="logo">
+					<Link to="./" onClick={closeMenu}>
+						<img src={chota_logo} alt="logo1" />
+					</Link>
+				</div>
+			) : null}
+			<nav className={menuOpen ? 'menu-open' : 'menu-closed'}>
+				<div
+					className="menu"
+					onClick={() => {
+						setMenuOpen(!menuOpen);
+					}}
+				>
+					{menuOpen ? (
+						<ImCross className='cross' />
+					) : (
+						<>
+							<span className={menuOpen ? '' : 'ham'}></span>
+							<span className={menuOpen ? '' : 'ham'}></span>
+							<span className={menuOpen ? '' : 'ham'}></span>
+						</>
+					)}
+				</div>
+				<ul className={menuOpen ? "open" : ""}>{listItems}</ul>
+			</nav>
+		</>
+	);
+}
