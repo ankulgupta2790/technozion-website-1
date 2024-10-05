02308795b19030ec25959158b00189f7efcecf1a
Anubhav <vaishnav.anubhav20@gmail.com>
2024-10-06 00:32:22 +0530

    reverted back to update home page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,20 +1,16 @@
-// import React from 'react';
+import React from 'react';
 import './about.css';
 import logo1 from './logo1.png';
 import AboutCard from './AboutCard';
-import { WebCanvas } from '../bg_animation/bg_animate.js';
 
 export const About = () => {
   return (
-    <div className="relative">
-      {/* Add the WebCanvas component for the background animation */}
-      <WebCanvas />
-
-      <div className="w-full z-1 relative p-4 text-white">
+    <div className="">
+      <div className="w-full p-4 bg-gradient-to-t to-black text-white">
         <AboutCard 
           image={logo1} 
           imgToRight={true} 
-          title="About" 
+          title="About Technozion" 
           content="Technozion, NIT Warangal's annual technical festival, started as a platform for students to showcase their technical skills and innovations. Now, after many successful editions, it has become one of the most anticipated technical fests in the country, attracting participants from various institutions. Throughout its history, Technozion has hosted renowned speakers and experts, providing valuable insights and inspiration to attendees. The festival features various competitions, workshops, and exhibitions, creating a vibrant atmosphere of learning and collaboration."
         />
       </div>
