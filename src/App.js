6912db483d9bb9a3a2ff225f596b4ae3a479e649
vikramcsb <vikramkhedar123@gmail.com>
2024-10-08 18:55:01 +0530

    updated footer and spotlight file

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
                 <AuthProvider>
                     <Navbar />
                     <RoutesManager />
-                    <Footer />
+                    {/* <Footer /> */}
                 </AuthProvider>
             )}
         </>
